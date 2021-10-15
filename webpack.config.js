a83cc92a9478717bcfd0a10933dec700486fe713
Robert <soarerobert17@gmail.com>
2021-10-15 12:44:45 +0300

    feat: build everything from one file

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const glob = require( 'glob' );
+const path = require( 'path' );
+
+module.exports = [
+	{
+		// OTTER DASHBOARD
+		...defaultConfig,
+		stats: 'minimal',
+		mode: NODE_ENV,
+		entry: {
+			index: './src/dashboard/index.js'
+		},
+		output: {
+			path: path.resolve( __dirname, './build/dashboard' ),
+		},
+	},
+	{
+		// OTTER BLOCKS
+		...defaultConfig,
+		stats: 'minimal',
+		mode: NODE_ENV,
+		entry: {
+			blocks: [
+				'./src/blocks/index.js',
+				'./src/blocks/plugins/registerPlugin.js',
+				...glob.sync( './src/blocks/blocks/**/index.js' )
+			],
+			'leaflet-map': './src/blocks/frontend/leaflet-map/index.js',
+			maps: './src/blocks/frontend/google-map/index.js',
+			slider: './src/blocks/frontend/slider/index.js',
+			'progress-bar': './src/blocks/frontend/progress-bar/index.js',
+			'circle-counter': './src/blocks/frontend/circle-counter/index.js',
+			lottie: './src/blocks/frontend/lottie/index.js',
+			tabs: './src/blocks/frontend/tabs/index.js',
+			masonry: './src/blocks/frontend/masonry/index.js',
+			form: './src/blocks/frontend/form/index.js',
+			countdown: './src/blocks/frontend/countdown/index.js',
+			popup: './src/blocks/frontend/popup/index.js'
+		},
+		output: {
+			path: path.resolve( __dirname, './build/blocks' ),
+			filename: '[name].js',
+			chunkFilename: 'chunk-[name].js'
+		},
+	}
+];
