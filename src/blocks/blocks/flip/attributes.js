5418fc97693ba271eac8a0b3bae6ab65d5b0405e
Robert <soarerobert17@gmail.com>
2021-10-22 16:08:17 +0300

    feat: add inspector with basic properties

--- src/blocks/blocks/flip/attributes.js
+++ src/blocks/blocks/flip/attributes.js
@@ -1,6 +1,21 @@
 const attributes = {
 	id: {
 		type: 'string'
+	},
+	width: {
+		type: 'number'
+	},
+	height: {
+		type: 'number'
+	},
+	paddings: {
+		type: 'number'
+	},
+	borderRadius: {
+		type: 'number'
+	},
+	backgroundColor: {
+		type: 'string'
 	}
 };
 
--- src/blocks/blocks/flip/save.js
+++ src/blocks/blocks/flip/save.js
@@ -13,8 +13,20 @@ const Save = ({
 	className
 }) => {
 	return (
-		<div className={className}>
+		<div className={className, 'anim'}>
+			<div
+				className="o-inner"
+			>
+				<div className="o-front">
+					<h1>FRONT</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris laoreet tempor ante, ac consequat nisl luctus nec. Etiam eu pellentesque tortor. Vivamus lobortis vitae tortor.</p>
+				</div>
+				<div className="o-back">
+					<InnerBlocks.Content
 
+					/>
+				</div>
+			</div>
 		</div>
 	);
 };
