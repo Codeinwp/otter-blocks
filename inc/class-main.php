143398acf5dd2d4201c308bf3a963edc473d6105
Robert <soarerobert17@gmail.com>
2021-10-25 16:18:37 +0300

    feat: shemlessly copy neve filter for adding async

--- inc/class-main.php
+++ inc/class-main.php
@@ -144,6 +144,7 @@ class Main {
 		add_action( 'init', array( $this, 'register_blocks' ), 11 );
 		add_action( 'enqueue_block_editor_assets', array( $this, 'enqueue_block_editor_assets' ), 1 );
 		add_action( 'enqueue_block_assets', array( $this, 'enqueue_block_frontend_assets' ) );
+		add_filter( 'script_loader_tag', array( $this, 'filter_script_loader_tag' ), 10, 2 );
 
 		add_action(
 			'get_footer',
@@ -1084,6 +1085,34 @@ class Main {
 		$symbol = isset( $symbols[ $currency ] ) ? $symbols[ $currency ] : '&#36;';
 
 		return $symbol;
+	}
+
+	/**
+	 * Adds async/defer attributes to enqueued / registered scripts.
+	 *
+	 * If #12009 lands in WordPress, this function can no-op since it would be handled in core.
+	 *
+	 * @link https://core.trac.wordpress.org/ticket/12009
+	 *
+	 * @param string $tag The script tag.
+	 * @param string $handle The script handle.
+	 *
+	 * @return string Script HTML string.
+	 */
+	public function filter_script_loader_tag( $tag, $handle ) {
+		foreach ( array( 'async', 'defer' ) as $attr ) {
+			if ( ! wp_scripts()->get_data( $handle, $attr ) ) {
+				continue;
+			}
+			// Prevent adding attribute when already added in #12009.
+			if ( ! preg_match( ":\s$attr(=|>|\s):", $tag ) ) {
+				$tag = preg_replace( ':(?=></script>):', " $attr", $tag, 1 );
+			}
+			// Only allow async or defer, not both.
+			break;
+		}
+
+		return $tag;
 	}
 
 	/**
