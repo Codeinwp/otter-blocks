c50ae99066e3494efe71e1abc2062c4ae5783fa5
Robert <soarerobert17@gmail.com>
2021-10-27 12:38:35 +0300

    chore: useless tweak

--- inc/class-blocks-animation.php
+++ inc/class-blocks-animation.php
@@ -104,7 +104,7 @@ class Blocks_Animation {
 			$asset_file['version'],
 			true
 		);
-		wp_script_add_data( 'themeisle-gutenberg-animation-frontend', 'defer', true );
+		wp_script_add_data( 'themeisle-gutenberg-animation-frontend', 'sync', true );
 	}
 
 	/**
