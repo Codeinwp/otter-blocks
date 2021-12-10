6c1a8072d0accba55097516e06780d164a471569
Hardeep Asrani <hardeepasrani@gmail.com>
2021-12-11 03:13:06 +0530

    Convert Circular Counter  block to V2

--- src/blocks/blocks/circle-counter/edit.js
+++ src/blocks/blocks/circle-counter/edit.js
@@ -1,9 +1,4 @@
 /**
- * External dependencies
- */
-import classnames from 'classnames';
-
-/**
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
@@ -19,7 +14,10 @@ import {
 	useRef
 } from '@wordpress/element';
 
-import { RichText } from '@wordpress/block-editor';
+import {
+	RichText,
+	useBlockProps
+} from '@wordpress/block-editor';
 
 /**
  * Internal dependencies
@@ -37,7 +35,6 @@ const CircularProgressBarBlock = ({
 	toggleSelection,
 	className
 }) => {
-
 	useEffect( () => {
 		const unsubscribe = blockInit( clientId, defaultAttributes );
 		return () => unsubscribe( attributes.id );
@@ -113,6 +110,11 @@ const CircularProgressBarBlock = ({
 		setAttributes({ title: value });
 	};
 
+	const blockProps = useBlockProps({
+		id: attributes.id,
+		className
+	});
+
 	return (
 		<Fragment>
 			<Inspector
@@ -121,10 +123,7 @@ const CircularProgressBarBlock = ({
 				onHeightChange={ onHeightChange }
 			/>
 
-			<div
-				className={ classnames( className ) }
-				id={ attributes.id }
-			>
+			<div { ...blockProps }>
 				{ ( 'default' === attributes.titleStyle ) && (
 					<div className="wp-block-themeisle-blocks-circle-counter-title__area">
 						<RichText
--- src/blocks/blocks/circle-counter/save.js
+++ src/blocks/blocks/circle-counter/save.js
@@ -1,19 +1,26 @@
+/**
+ * WordPress dependencies
+ */
+import { useBlockProps } from '@wordpress/block-editor';
+
 const Save = ({
 	attributes,
 	className
 }) => {
+	const blockProps = useBlockProps.save({
+		id: attributes.id,
+		className,
+		'data-percentage': attributes.percentage,
+		'data-duration': attributes.duration,
+		'data-height': attributes.height,
+		'data-stroke-width': attributes.strokeWidth,
+		'data-font-size-percent': attributes.fontSizePercent,
+		'data-background-stroke': attributes.backgroundColor,
+		'data-progress-stroke': attributes.progressColor
+	});
+
 	return (
-		<div
-			className={ className }
-			id={ attributes.id }
-			data-percentage={ attributes.percentage }
-			data-duration={ attributes.duration }
-			data-height={ attributes.height }
-			data-stroke-width={ attributes.strokeWidth }
-			data-font-size-percent={ attributes.fontSizePercent }
-			data-background-stroke={ attributes.backgroundColor }
-			data-progress-stroke={ attributes.progressColor }
-		>
+		<div { ...blockProps }>
 			{ ( 'default' === attributes.titleStyle ) && (
 				<div className="wp-block-themeisle-blocks-circle-counter-title__area">
 					<span className="wp-block-themeisle-blocks-circle-counter-title__value">
