da7298672a7bbb9428b6837e3e58b94861be82fb
Robert <soarerobert17@gmail.com>
2021-12-14 16:08:40 +0200

    feat: register the custom tag format

---
+++ src/blocks/plugins/count/index.js
@@ -0,0 +1,47 @@
+/**
+ * External dependencies.
+ */
+import { brush } from '@wordpress/icons';
+
+/**
+ * WordPress dependencies.
+ */
+import { __ } from '@wordpress/i18n';
+
+import {
+	registerFormatType,
+	toggleFormat
+} from '@wordpress/rich-text';
+
+import {
+	RichTextShortcut,
+	RichTextToolbarButton
+} from '@wordpress/block-editor';
+
+import { Fragment } from '@wordpress/element';
+import './editor.scss';
+
+const name = 'themeisle-blocks/count-animation';
+
+registerFormatType( name, {
+	title: __( 'Count Animation', 'otter-blocks' ),
+	tagName: 'o-anim-count',
+	className: null,
+
+	edit: ({ isActive, value, onChange }) => {
+		const onToggle = () => onChange( toggleFormat( value, { type: name }) );
+
+		// TODO: Apply only for text with numbers
+
+		return (
+			<Fragment>
+				<RichTextToolbarButton
+					icon={ brush }
+					title={ __( 'Apply Count Animation', 'otter-blocks' ) }
+					onClick={ onToggle }
+					isActive={ isActive }
+				/>
+			</Fragment>
+		);
+	}
+});
