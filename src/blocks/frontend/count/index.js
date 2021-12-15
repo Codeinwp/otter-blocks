d35296d9c3cd46f9c043891efb4ccbabeb1aaf5b
Robert <soarerobert17@gmail.com>
2021-12-15 15:43:39 +0200

    feat: add animation count

--- src/blocks/frontend/count/index.js
+++ src/blocks/frontend/count/index.js
@@ -2,9 +2,89 @@
  * WordPress dependencies
  */
 import domReady from '@wordpress/dom-ready';
+import { range } from '../../helpers/helper-functions';
 
+const makeInterval = ( duration, deltaTime ) => {
+
+	let interval;
+	let currentStep = 0;
+	const steps = ( Math.ceil( duration / deltaTime ) + 1 ) || 0;
+
+	const stop = ( callback ) => {
+		clearInterval( interval );
+		callback?.();
+	};
+
+	const start = ( callback, endCallback ) => {
+		interval = setInterval( () => {
+			if ( currentStep < steps ) {
+				callback?.( currentStep );
+				currentStep += 1;
+			} else {
+				stop( endCallback );
+			}
+		}, deltaTime * 1000 );
+	};
+
+	return {
+		steps,
+		start,
+		stop
+	};
+};
+
+/**
+ *
+ * @param {HTMLDivElement} elem
+ */
+const initCount = ( elem ) => {
+	const text = elem?.innerHTML || '';
+	const len = text.length;
+	const formatElements = Array.from( text ).reverse().map( ( c, idx ) => {
+		if ( ! ' ,'.includes( c ) ) {
+			return null;
+		}
+
+		return {
+			position: idx,
+			character: c
+		};
+	}).filter( x => x );
+	const numAfterComma = text?.split( '.' )?.[1]?.length || 0;
+	const number = parseFloat( text.replace( /[\s,]/g, '' ) );
+
+	/**
+	 *
+	 * @param {number} n
+	 */
+	const applyFormat = ( n ) => {
+		const num = ( numAfterComma ? n.toFixed( numAfterComma ) : n ).toString().split( '' ).reverse();
+
+		formatElements.forEach( f => {
+			if ( f.position < num.length ) {
+				num.splice( f.position, 0, f.character );
+			}
+		});
+
+		return num.reverse().join( '' ).padStart( len, ' ' );
+	};
+
+	const { start, steps } = makeInterval( 3, 0.2 );
+	const delta = Math.round( number /  steps );
+	const values = range( 0, number, delta );
+	values[steps - 1] =  number;
+
+	elem.style.whiteSpace = 'pre';
+	start( ( i ) => {
+		elem.innerHTML = applyFormat( values[i]);
+	}, () => {
+		elem.style.whiteSpace = '';
+	});
+};
 
 domReady( () => {
 	const anims = document.querySelectorAll( 'o-anim-count' );
-	console.log( anims );
+	anims.forEach( ( elem ) => {
+		initCount( elem );
+	});
 });
