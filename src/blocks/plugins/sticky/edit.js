3564a5a24e617dfa30ba3dfc87164d2e6b9221ee
Robert <soarerobert17@gmail.com>
2022-02-09 12:41:15 +0200

    feat: Sticky To based on the available containers

--- src/blocks/plugins/sticky/edit.js
+++ src/blocks/plugins/sticky/edit.js
@@ -13,6 +13,11 @@ import {
 	ToggleControl
 } from '@wordpress/components';
 
+import {
+	useState,
+	useEffect
+} from '@wordpress/element';
+
 const FILTER_OPTIONS = {
 	position: 'o-sticky-pos',
 	offset: 'o-sticky-offset',
@@ -21,10 +26,62 @@ const FILTER_OPTIONS = {
 	usage: 'o-sticky-use'
 };
 
+/**
+ * Get the container for the given element
+ * @param {HTMLDivElement} elem The sticky element
+ * @return {HTMLDivElement} The parent container. Return `body` as default
+ */
+const getStickyContainer = ( elem, scope ) => {
+	let parent = elem?.parentElement;
+	const sections = [];
+	while ( parent ) {
+		if (
+			(
+				parent.classList.contains( 'wp-block-themeisle-blocks-advanced-column' ) ||
+				parent.classList.contains( 'wp-block-group' ) ||
+				parent.classList.contains( 'wp-block-column' )
+			) &&
+			'o-sticky-scope-parent' === scope
+		) {
+			return parent;
+		}
+		if (
+			parent.classList.contains( 'wp-block-themeisle-blocks-advanced-columns' ) ||
+			parent.classList.contains( 'wp-block-group' ) ||
+			parent.classList.contains( 'wp-block-columns' )
+		) {
+			if ( 'o-sticky-scope-section' === scope ) {
+				return parent;
+			} else if ( 'o-sticky-scope-main-area' === scope ) {
+				sections.push( parent );
+			}
+		}
+		parent = parent.parentElement;
+	}
+	return 'o-sticky-scope-main-area' === scope ? sections.pop() : document.body;
+};
+
 const Edit = ({
 	attributes,
-	setAttributes
+	setAttributes,
+	clientId
 }) => {
+
+	console.log( attributes, {clientId});
+
+	const [ containerOptions, setContainerOptions ] = useState([ {
+		label: __( 'Screen', 'otter-blocks' ),
+		value: 'o-sticky-scope-screen'
+	} ]);
+
+	/*
+s		E.g:
+			Position -> 'o-sticky-pos-top'
+			Top Offset -> 'o-sticky-offset--40' where 40 will be value (it will be extracted by the frontend script)
+
+		And element with the classe like this 'o-sticky o-sticky-pos-bottom o-sticky-pos-bottom-30', it will be a sticky element with 30px distance from the bottom of the window viewport
+	*/
+
 	const position = attributes?.className?.includes( 'o-sticky-pos-bottom' ) ? 'o-sticky-pos-bottom' : 'o-sticky-pos-top';
 	const limit = attributes?.className?.split( ' ' ).filter( c => c.includes( 'o-sticky-scope' ) ).pop() || 'o-sticky-scope-main-area';
 	const behaviour = attributes?.className?.split( ' ' ).filter( c => c.includes( 'o-sticky-bhvr' ) ).pop() || 'o-sticky-bhvr-keep';
@@ -47,13 +104,68 @@ const Edit = ({
 			}, 40 ) );
 	};
 
-	/*
-s		E.g:
-			Position -> 'o-sticky-pos-top'
-			Top Offset -> 'o-sticky-offset--40' where 40 will be value (it will be extracted by the frontend script)
+	useEffect( () => {
+		if ( clientId ) {
+			const block = document.querySelector( `#block-${clientId}` );
+			if ( block ) {
+				let parent = block?.parentElement;
+				const containers = [];
+				const options = [];
+				while ( parent && ! parent.classList.contains( 'is-root-container' ) ) {
+					if (
+						(
+							parent.classList.contains( 'wp-block-themeisle-blocks-advanced-column' ) ||
+							parent.classList.contains( 'wp-block-group' ) ||
+							parent.classList.contains( 'wp-block-column' )
+						)
+					) {
+						containers.push( 'parent' );
+					} if (
+						(
+							parent.classList.contains( 'wp-block-themeisle-blocks-advanced-columns' ) ||
+							parent.classList.contains( 'wp-block-group' ) ||
+							parent.classList.contains( 'wp-block-columns' )
+						)
+					) {
+						containers.push( 'section' );
+					}
 
-		And element with the classe like this 'o-sticky o-sticky-pos-bottom o-sticky-pos-bottom-30', it will be a sticky element with 30px distance from the bottom of the window viewport
-	*/
+					parent = parent.parentElement;
+				}
+
+				console.log( containers );
+
+				if ( containers.some( x => 'section' === x ) ) {
+					options.push({
+						label: __( 'Top Level Block', 'otter-blocks' ),
+						value: 'o-sticky-scope-main-area'
+					});
+					if ( 1 < containers.filter( x => 'section' === x ) ) {
+						options.push({
+							label: __( 'Section', 'otter-blocks' ),
+							value: 'o-sticky-scope-section'
+						});
+					}
+				}
+
+				if ( containers.some( x => 'parent' === x ) ) {
+					options.push({
+						label: __( 'Parent Block', 'otter-blocks' ),
+						value: 'o-sticky-scope-parent'
+					});
+				}
+
+				options.push({
+					label: __( 'Screen', 'otter-blocks' ),
+					value: 'o-sticky-scope-screen'
+				});
+
+				setContainerOptions( options );
+			}
+
+		}
+	}, [ clientId ]);
+
 
 	return (
 		<InspectorControls>
@@ -65,24 +177,7 @@ s		E.g:
 					label={ __( 'Sticky To', 'otter-blocks' ) }
 					help={ __( 'Stick to a group, columns, section block, or screen to limit the movement. The limit will differ from block to block, except for the blocks with the same parent.', 'otter-blocks' ) }
 					value={ limit }
-					options={ [
-						{
-							label: __( 'Top Level Block', 'otter-blocks' ),
-							value: 'o-sticky-scope-main-area'
-						},
-						{
-							label: __( 'Parent Block', 'otter-blocks' ),
-							value: 'o-sticky-scope-parent'
-						},
-						{
-							label: __( 'Section', 'otter-blocks' ),
-							value: 'o-sticky-scope-section'
-						},
-						{
-							label: __( 'Screen', 'otter-blocks' ),
-							value: 'o-sticky-scope-screen'
-						}
-					] }
+					options={ containerOptions }
 					onChange={ value => addOption( value, FILTER_OPTIONS.scope ) }
 				/>
 
