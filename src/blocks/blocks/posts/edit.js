6585833859bdd427bbccd34ea87587a857b1f0fa
Robert <soarerobert17@gmail.com>
2022-02-25 10:36:05 +0200

    feat(editor): set text align CSS var

--- src/blocks/blocks/posts/edit.js
+++ src/blocks/blocks/posts/edit.js
@@ -120,7 +120,7 @@ const Edit = ({
 						.pop()
 				);
 			} else if ( attributes.featuredPost ) {
-				setFeatured( posts?.find( post => post.id.toString() == attributes.featuredPost ) );
+				setFeatured( posts?.find( post => post?.id?.toString() == attributes.featuredPost ) );
 			} else {
 				setFeatured( '' );
 			}
@@ -130,6 +130,7 @@ const Edit = ({
 	const fontSizeStyle = css`
 		${ attributes.imageWidth && `--img-width: ${ attributes.imageWidth }px;` }
 		--vert-align: ${_align( attributes.verticalAlign )};
+		--text-align: ${_align( attributes.textAlign )};
 
 		@media ( min-width: 960px ) {
 			${ attributes.customTitleFontSize && `--title-text-size: ${ attributes.customTitleFontSize }px;` }
