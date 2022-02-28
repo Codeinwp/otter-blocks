ae6da9590bd7c11124e716dab1bf37f99be48751
Robert <soarerobert17@gmail.com>
2022-02-28 16:18:30 +0200

    feat: show ACF upgrade only to Neve User

--- src/blocks/blocks/posts/components/design-layout-builder.js
+++ src/blocks/blocks/posts/components/design-layout-builder.js
@@ -71,58 +71,65 @@ const LayoutBuilder = ({
 					lockAxis="y"
 				/>
 
-				{ window?.acf === undefined ? (
-					<ExternalLink
-						href="https://wordpress.org/plugins/advanced-custom-fields/"
-						target="_blank"
-					>
-						{__( 'Activate Advance Custom Fields to add more fields.', 'otter-blocks' )}
-					</ExternalLink>
-				) : (
-					<Fragment>
-						<Button
-							variant="secondary"
-							isSecondary
-							className="o-conditions__add"
-							disabled={ window?.acf === undefined || ! window?.themeisleGutenberg?.hasNeveSupport?.hasNevePro}
-							onClick={ () => {
-								let id = uuidv4().slice( 0, 8 );
-								while ( 0 < attributes?.customMetas?.some( ({ otherId }) => otherId === id )  ) {
-									id = uuidv4().slice( 0, 8 );
-								}
-								id = `custom_${ id }`;
-
-								const newMeta = {
-									id,
-									field: '',
-									display: true
-								};
-
-								const {
-									template,
-									customMetas
-								} = filterDeadCustomTemplates();
-
-								setAttributes({
-									template: [ ...template, id ],
-									customMetas: customMetas ? [ ...customMetas, newMeta ] : [ newMeta ]
-								});
-							}}
-						>
-							{ ! window.themeisleGutenberg?.hasNeveSupport?.hasNevePro ? __( 'You need Neve Pro!', 'otter-blocks' ) : __( 'Add Meta Field', 'otter-blocks' ) }
-						</Button>
-						{
-							( ! window.themeisleGutenberg?.hasNeveSupport?.hasNevePro ) && (
+				{
+					window.themeisleGutenberg?.hasNeveSupport?.hasNeve && (
+						<Fragment>
+							{ window?.acf === undefined ? (
 								<ExternalLink
-									href="https://themeisle.com/themes/neve/"
+									href="https://wordpress.org/plugins/advanced-custom-fields/"
 									target="_blank"
 								>
-									{__( 'Learn more about Neve.', 'otter-blocks' )}
+									{__( 'Activate Advance Custom Fields to add more fields.', 'otter-blocks' )}
 								</ExternalLink>
-							)
-						}
-					</Fragment>
-				) }
+							) : (
+								<Fragment>
+									<Button
+										variant="secondary"
+										isSecondary
+										className="o-conditions__add"
+										disabled={ window?.acf === undefined || ! window?.themeisleGutenberg?.hasNeveSupport?.hasNevePro}
+										onClick={ () => {
+											let id = uuidv4().slice( 0, 8 );
+											while ( 0 < attributes?.customMetas?.some( ({ otherId }) => otherId === id )  ) {
+												id = uuidv4().slice( 0, 8 );
+											}
+											id = `custom_${ id }`;
+
+											const newMeta = {
+												id,
+												field: '',
+												display: true
+											};
+
+											const {
+												template,
+												customMetas
+											} = filterDeadCustomTemplates();
+
+											setAttributes({
+												template: [ ...template, id ],
+												customMetas: customMetas ? [ ...customMetas, newMeta ] : [ newMeta ]
+											});
+										}}
+									>
+										{ __( 'Add Meta Field', 'otter-blocks' ) }
+									</Button>
+									{
+										( ! window.themeisleGutenberg?.hasNeveSupport?.hasNevePro ) && (
+											<ExternalLink
+												href="https://themeisle.com/themes/neve/"
+												target="_blank"
+											>
+												{__( 'Unlock the option to add data from Advance Custom Fields into the posts display. Upgrade to Neve Pro.', 'otter-blocks' )}
+											</ExternalLink>
+										)
+									}
+								</Fragment>
+							) }
+						</Fragment>
+					)
+				}
+
 			</div>
 		</Fragment>
 	);
