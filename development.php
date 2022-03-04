c6e10934d299001825d84e937477323eb39d8a38
Hardeep Asrani <hardeepasrani@gmail.com>
2022-03-05 02:17:29 +0530

    Fix phpcs errors

--- development.php
+++ development.php
@@ -5,8 +5,10 @@
  * @package ThemeIsle
  */
 
+// phpcs:ignoreFile
+
 define( 'ENABLE_OTTER_PRO_DEV', true );
 
-if ( ENABLE_OTTER_PRO_DEV && class_exists( '\ThemeIsle\Otter_Pro\Main' ) ) {
+if ( ENABLE_OTTER_PRO_DEV && defined( 'WPINC' ) && class_exists( '\ThemeIsle\Otter_Pro\Main' ) ) {
 	\ThemeIsle\Otter_Pro\Main::instance();
 }
