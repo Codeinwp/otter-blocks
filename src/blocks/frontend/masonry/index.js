386f3aa8c812de12fc87ed66ba7bbbcfc8214d09
Robert <soarerobert17@gmail.com>
2022-03-28 15:52:53 +0300

    fix: Masonry inside Tabs block

--- src/blocks/frontend/masonry/index.js
+++ src/blocks/frontend/masonry/index.js
@@ -6,35 +6,57 @@ import domReady from '@wordpress/dom-ready';
 domReady( () => {
 	const galleries = document.getElementsByClassName( 'otter-masonry' );
 
+	const options = {
+		root: null,
+		rootMargin: '0px',
+		threshold: [ 0.0 ]
+	};
+
 	Array.from( galleries ).forEach( gallery => {
-		const container = gallery.querySelector( '.wp-block-gallery' );
-		const pattern = /columns-(\d)/;
-
-		const margin = gallery.dataset.margin !== undefined ? Number( gallery.dataset.margin ) : 10;
-
-		let columns = Array.from( container.classList ).find( className => {
-			const res = pattern.exec( className );
-			if ( null !== res ) {
-				return true;
-			}
-		});
-
-		columns = pattern.exec( columns );
-		columns = columns ? Number( columns[1]) : 3;
-
-		container.removeAttribute( 'class' );
 
 		const checker = setInterval( () => {
-			if ( window?.Macy ) {
-				window.Macy({
-					container: gallery.querySelector( '.blocks-gallery-grid' ) || container,
-					trueOrder: false,
-					waitForImages: false,
-					margin,
-					columns
-				});
+			const container = gallery.querySelector( '.wp-block-gallery' );
+
+			if (window?.Macy) {
+				const targetContainer = gallery.querySelector( '.blocks-gallery-grid' ) || container;
+
+				const observer = new IntersectionObserver( entries => {
+					entries.forEach( entry => {
+						if (entry.isIntersecting && 0 <= entry.intersectionRect.height) {
+							const pattern = /columns-(\d)/;
+
+							const margin = gallery.dataset.margin !== undefined ? Number( gallery.dataset.margin ) : 10;
+
+							let columns = Array.from( container.classList ).find( className => {
+								const res = pattern.exec( className );
+								if (null !== res) {
+									return true;
+								}
+							} );
+
+							columns = pattern.exec( columns );
+							columns = columns ? Number( columns[1] ) : 3;
+
+							container.removeAttribute( 'class' );
+
+							console.log("Initiate")
+
+							window.Macy( {
+								container: targetContainer,
+								trueOrder: false,
+								waitForImages: false,
+								margin,
+								columns
+							} );
+
+							observer.unobserve( targetContainer );
+						}
+					} );
+				}, options );
+
+				observer.observe( targetContainer );
 				clearInterval( checker );
 			}
 		}, 1_500 );
-	});
-});
+	} );
+} );
