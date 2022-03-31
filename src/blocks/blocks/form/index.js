fe65a4a5a3cd88487c050ed9aaf2df47a3c35e39
Robert <soarerobert17@gmail.com>
2022-03-31 14:49:32 +0300

    feat: add submit button styling

--- src/blocks/blocks/form/index.js
+++ src/blocks/blocks/form/index.js
@@ -29,6 +29,21 @@ registerBlockType( name, {
 	edit,
 	save,
 	deprecated,
+	styles: [
+		{
+			name: 'default',
+			label: __( 'Default', 'otter-blocks' ),
+			isDefault: true
+		},
+		{
+			name: 'left',
+			label: __( 'Left', 'otter-blocks' )
+		},
+		{
+			name: 'full',
+			label: __( 'Full', 'otter-blocks' )
+		}
+	],
 	variations: [
 		{
 			name: 'themeisle-blocks/form-contact',
