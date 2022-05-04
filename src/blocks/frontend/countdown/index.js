06fb64a467921bfcd2a42b85e60260519b4f0aa9
Hardeep Asrani <hardeepasrani@gmail.com>
2022-05-04 19:31:03 +0530

    Remove deps

--- src/blocks/frontend/countdown/index.js
+++ src/blocks/frontend/countdown/index.js
@@ -1,11 +1,4 @@
 /**
- * WordPress dependencies
- */
-import moment from 'moment';
-import { sprintf, __ } from '@wordpress/i18n';
-import { __experimentalGetSettings } from '@wordpress/date';
-
-/**
  * Internal dependencies
  */
 import { domReady } from '../../helpers/frontend-helper-functions.js';
@@ -30,26 +23,27 @@ export const getIntervalFromUnix = ( unixTime, settings ) => {
 	const hours = Math.floor( unixTime / _MS_PER_HOURS % 24 );
 	const minutes = Math.floor( unixTime / _MS_PER_MINUTES % 60 );
 	const seconds = Math.floor( unixTime / _MS_PER_SECONDS % 60 );
+	const { i18n } = window.themeisleGutenbergCountdown;
 
 	const time = [
 		{
 			tag: 'day',
-			name: 1 < days ? __( 'Days', 'otter-blocks' ) : __( 'Day', 'otter-blocks' ),
+			name: 1 < days ? i18n.days : i18n.day,
 			value: days
 		},
 		{
 			tag: 'hour',
-			name: 1 < hours ? __( 'Hours', 'otter-blocks' ) : __( 'Hour', 'otter-blocks' ),
+			name: 1 < hours ? i18n.hours : i18n.hour,
 			value: hours
 		},
 		{
 			tag: 'minute',
-			name: 1 < minutes ? __( 'Minutes', 'otter-blocks' ) : __( 'Minute', 'otter-blocks' ),
+			name: 1 < minutes ? i18n.minutes : i18n.minute,
 			value: minutes
 		},
 		{
 			tag: 'second',
-			name: 1 < seconds ? __( 'Seconds', 'otter-blocks' ) : __( 'Second', 'otter-blocks' ),
+			name: 1 < seconds ? i18n.seconds : i18n.second,
 			value: seconds
 		}
 	]
@@ -62,16 +56,6 @@ export const getIntervalFromUnix = ( unixTime, settings ) => {
 		});
 
 	return time;
-};
-
-// Get site's timezone.
-export const getTimezone = () => {
-	const settings = __experimentalGetSettings();
-	const offset   = 60 * settings.timezone.offset;
-	const sign     = 0 > offset ? '-' : '+';
-	const absmin   = Math.abs( offset );
-	const timezone = sprintf( '%s%02d:%02d', sign, absmin / 60, absmin % 60 );
-	return timezone;
 };
 
 /**
@@ -106,8 +90,9 @@ const getComponentsUpdate = ( root ) => {
  * @returns {Function} Function that update the countdown every time it is called. You can send a callback to be triggered when is finised.
  */
 const updateTime = ( date, updateComponents ) => {
-	let _date = date + getTimezone();
-	_date = moment( _date ).unix() * 1000;
+	let _date = date + window.themeisleGutenbergCountdown.timezone;
+	_date = new Date( _date );
+	_date = _date.getTime();
 	return ( onFinishCb ) => {
 		const time = getIntervalFromUnix( _date - Date.now() );
 		time.forEach( ({ tag, value, name }) => {
