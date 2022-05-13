0ea5cc68237fa533a3a920dd00a17a5276005fae
Hardeep Asrani <hardeepasrani@gmail.com>
2022-05-14 02:13:58 +0530

    Fix Upsell font & replace Otter-visibility notice

--- src/blocks/index.js
+++ src/blocks/index.js
@@ -1,14 +1,31 @@
 /**
  * WordPRess dependencies
  */
+import { __ } from '@wordpress/i18n';
+
 import { updateCategory } from '@wordpress/blocks';
+
+import { Icon } from '@wordpress/components';
+
+import { addFilter } from '@wordpress/hooks';
 
 /**
  * Internal dependencies
  */
 import './style.scss';
 import './editor.scss';
-import { otterIconColored as icon } from './helpers/icons.js';
+import {
+	otterIcon,
+	otterIconColored as icon
+} from './helpers/icons.js';
 
 updateCategory( 'themeisle-blocks', { icon });
 updateCategory( 'themeisle-woocommerce-blocks', { icon });
+
+const PoweredBy = () => {
+	return (
+		<div className="o-is-powered">{ __( 'Powered by Otter', 'otter-blocks' ) } { <Icon icon={ otterIcon } /> }</div>
+	);
+};
+
+addFilter( 'otter.poweredBy', 'themeisle-gutenberg/powered-by-notice', PoweredBy );
