89173cb985a2396c872113d8f41617458ccb486f
arinaturcu <arinaturcu2@gmail.com>
2022-07-11 19:07:41 +0300

    fix: icon in frontent

--- inc/css/blocks/class-accordion-css.php
+++ inc/css/blocks/class-accordion-css.php
@@ -245,7 +245,7 @@ class Accordion_CSS extends Base_CSS {
 						'property'  => 'content',
 						'value'     => 'icon',
 						'format'    => function( $value ) use ( $fa_icons ) {
-							return '"\\' . $fa_icons[ $value['name'] ]['unicode'] . '"';
+							return '"' . str_replace( 'f', '\\\f', $fa_icons[ $value['name'] ]['unicode'] ) . '"';
 						},
 						'condition' => function( $attrs ) {
 							return isset( $attrs['icon'] );
@@ -283,7 +283,7 @@ class Accordion_CSS extends Base_CSS {
 						'property'  => 'content',
 						'value'     => 'openItemIcon',
 						'format'    => function( $value ) use ( $fa_icons ) {
-							return '"\\' . $fa_icons[ $value['name'] ]['unicode'] . '"';
+							return '"' . str_replace( 'f', '\\\f', $fa_icons[ $value['name'] ]['unicode'] ) . '"';
 						},
 						'condition' => function( $attrs ) {
 							return isset( $attrs['openItemIcon'] );
