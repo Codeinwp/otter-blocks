7c1ef348806fd2a579a3c16caa2caa6d18e7f8a8
Hardeep Asrani <hardeepasrani@gmail.com>
2022-08-01 17:08:56 +0530

    Load all custom CSS in footer

--- inc/css/class-block-frontend.php
+++ inc/css/class-block-frontend.php
@@ -55,8 +55,8 @@ class Block_Frontend extends Base_CSS {
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_google_fonts' ), 19 );
 		add_action( 'wp_head', array( $this, 'enqueue_google_fonts_backward' ), 19 );
 		add_filter( 'get_the_excerpt', array( $this, 'get_excerpt_end' ), 20 );
-		add_action( 'wp_head', array( $this, 'enqueue_widgets_css' ) );
-		add_action( 'wp_head', array( $this, 'enqueue_assets' ) );
+		add_action( 'wp_footer', array( $this, 'enqueue_widgets_css' ) );
+		add_action( 'wp_footer', array( $this, 'enqueue_assets' ) );
 		add_action( 'wp_footer', array( $this, 'enqueue_global_styles' ) );
 	}
 
@@ -307,30 +307,15 @@ class Block_Frontend extends Base_CSS {
 			$this->enqueue_reusable_styles( $blocks );
 		}
 
-		$total_inline_limit = 20000;
-		$total_inline_limit = apply_filters( 'styles_inline_size_limit', 20000 );
-
 		$wp_upload_dir = wp_upload_dir( null, false );
 		$basedir       = $wp_upload_dir['basedir'] . '/themeisle-gutenberg/';
 		$file_path     = $basedir . $file_name;
-		$file_size     = filesize( $file_path );
-
-		if ( $this->total_inline_size + $file_size < $total_inline_limit ) {
-			add_action(
-				'wp_footer',
-				function () use ( $post_id ) {
-					return $this->get_post_css( $post_id );
-				}
-			);
-
-			$this->total_inline_size += (int) $file_size;
-			return;
-		}
 
 		add_action(
 			'wp_footer',
-			function () use ( $post_id, $file_name, $file_url, $file_path ) {
+			function () use ( $file_name, $file_url, $file_path ) {
 				wp_enqueue_style( 'otter-' . $file_name, $file_url, array( 'otter-blocks' ), OTTER_BLOCKS_VERSION );
+				wp_style_add_data( 'otter-' . $file_name, 'path', $file_path );
 			}
 		);
 	}
