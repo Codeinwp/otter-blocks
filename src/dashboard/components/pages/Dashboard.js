e2af9be850374c17b4e758cc1301932286d6fb25
arinaturcu <arinaturcu2@gmail.com>
2023-01-16 10:40:07 +0200

    chore: move toggle

--- src/dashboard/components/pages/Dashboard.js
+++ src/dashboard/components/pages/Dashboard.js
@@ -92,16 +92,6 @@ const Dashboard = ({
 						onChange={ () => updateOption( 'themeisle_blocks_settings_block_conditions', ! Boolean( getOption( 'themeisle_blocks_settings_block_conditions' ) ) ) }
 					/>
 				</PanelRow>
-
-				<PanelRow>
-					<ToggleControl
-						label={ __( 'Highlight the Dynamic Text', 'otter-blocks' ) }
-						help={ __( 'Easily differentiate between dynamic and normal text in the editor.', 'otter-blocks' ) }
-						checked={ Boolean( getOption( 'themeisle_blocks_settings_highlight_dynamic' ) ) }
-						disabled={ 'saving' === status }
-						onChange={ () => updateOption( 'themeisle_blocks_settings_highlight_dynamic', ! Boolean( getOption( 'themeisle_blocks_settings_highlight_dynamic' ) ) ) }
-					/>
-				</PanelRow>
 			</PanelBody>
 
 			<PanelBody
@@ -144,6 +134,16 @@ const Dashboard = ({
 						checked={ Boolean( getOption( 'themeisle_blocks_settings_review_scale' ) ) }
 						disabled={ 'saving' === status }
 						onChange={ () => updateOption( 'themeisle_blocks_settings_review_scale', ! Boolean( getOption( 'themeisle_blocks_settings_review_scale' ) ) ) }
+					/>
+				</PanelRow>
+
+				<PanelRow>
+					<ToggleControl
+						label={ __( 'Highlight the Dynamic Text', 'otter-blocks' ) }
+						help={ __( 'Easily differentiate between dynamic and normal text in the editor.', 'otter-blocks' ) }
+						checked={ Boolean( getOption( 'themeisle_blocks_settings_highlight_dynamic' ) ) }
+						disabled={ 'saving' === status }
+						onChange={ () => updateOption( 'themeisle_blocks_settings_highlight_dynamic', ! Boolean( getOption( 'themeisle_blocks_settings_highlight_dynamic' ) ) ) }
 					/>
 				</PanelRow>
 
