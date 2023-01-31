57e91ebafd1eb5701a6c17ef4ce119eb1bcd63fd
Soare Robert Daniel <soare.robert.daniel@protonmail.com>
2023-01-31 13:31:25 +0200

    chore: CSS

--- src/blocks/blocks/tabs/group/save.js
+++ src/blocks/blocks/tabs/group/save.js
@@ -5,12 +5,18 @@ import {
 	InnerBlocks,
 	useBlockProps
 } from '@wordpress/block-editor';
+import classNames from 'classnames';
 
 const Save = ({
 	attributes
 }) => {
 	const blockProps = useBlockProps.save({
-		id: attributes.id
+		id: attributes.id,
+		className: classNames(
+			attributes.className,
+			{ 'has-pos-left': 'left' === attributes.tabsPosition  },
+			( attributes.titleAlignment && `is-align-${ attributes.titleAlignment }` )
+		)
 	});
 
 	return (
