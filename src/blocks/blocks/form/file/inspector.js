e9c6ea95f461e1dba200eb4470c691775b1000c3
Soare Robert Daniel <soare.robert.daniel@protonmail.com>
2023-03-06 16:06:08 +0200

    chore: add inspector pro hooks

--- src/blocks/blocks/form/file/inspector.js
+++ src/blocks/blocks/form/file/inspector.js
@@ -10,17 +10,77 @@ import {
 
 import {
 	Button,
+	Disabled,
+	ExternalLink,
 	PanelBody,
 	SelectControl,
 	TextControl,
 	ToggleControl
 } from '@wordpress/components';
 import { applyFilters } from '@wordpress/hooks';
-import { useContext } from '@wordpress/element';
+import { Fragment, useContext } from '@wordpress/element';
 
 import { HideFieldLabelToggle, switchFormFieldTo } from '../common';
 import { FormContext } from '../edit';
 import { Notice } from '../../../components';
+import { setUtm } from '../../../helpers/helper-functions';
+
+const ProPreview = ({ attributes }) => {
+	const setAttributes = () => {};
+
+	return (
+		<Fragment>
+			<Disabled>
+				<TextControl
+					label={ __( 'Max File Size in MB', 'otter-blocks' ) }
+					type="number"
+					value={ attributes.maxFileSize }
+					onChange={ maxFileSize => setAttributes({ maxFileSize }) }
+					help={ __( 'You may need to contact your hosting provider to increase file sizes.', 'otter-blocks' ) }
+				/>
+
+				<TextControl
+					label={ __( 'Allowed File Types', 'otter-blocks' ) }
+					value={ attributes.allowedFileTypes }
+					onChange={ allowedFileTypes => setAttributes({ allowedFileTypes }) }
+					help={ __( 'Allowed file types separated by coma. E.g.: .png, .jpg, .pdf, .doc', 'otter-blocks' ) }
+				/>
+
+				<TextControl
+					label={ __( 'Help Text', 'otter-blocks' ) }
+					value={ attributes.helpText }
+					onChange={ helpText => setAttributes({ helpText }) }
+				/>
+
+				<ToggleControl
+					label={ __( 'Required', 'otter-blocks' ) }
+					help={ __( 'If enabled, the input field must be filled out before submitting the form.', 'otter-blocks' ) }
+					checked={ attributes.isRequired }
+					onChange={ isRequired => setAttributes({ isRequired }) }
+				/>
+
+				<ToggleControl
+					label={ __( 'Allow multiple file uploads', 'otter-blocks' ) }
+					checked={ attributes.multipleFiles }
+					onChange={ multipleFiles => setAttributes({ multipleFiles }) }
+				/>
+
+				<ToggleControl
+					label={ __( 'Save to Media Library', 'otter-blocks' ) }
+					help={ __( 'If enabled, the files will be saved to Media Library instead of adding them as attachments to email.', 'otter-blocks' ) }
+					checked={ 'media-library' === attributes.saveFiles }
+					onChange={ value => setAttributes({ saveFiles: value ? 'media-library' : undefined }) }
+				/>
+			</Disabled>
+
+			{ ! Boolean( window.themeisleGutenberg?.hasPro ) && (
+				<Notice
+					notice={<ExternalLink href={setUtm( window.themeisleGutenberg.upgradeLink, 'formfilefieldfeature' )}>{__( 'Activate this field with Otter Pro.', 'otter-blocks' )}</ExternalLink>}
+					variant="upsell" instructions={undefined}				/>
+			) }
+		</Fragment>
+	);
+};
 
 /**
  *
@@ -109,52 +169,11 @@ const Inspector = ({
 
 				<HideFieldLabelToggle attributes={ attributes } setAttributes={ setAttributes } />
 
-				<TextControl
-					label={ __( 'Max File Size in MB', 'otter-blocks' ) }
-					type="number"
-					value={ attributes.maxFileSize }
-					onChange={ maxFileSize => setAttributes({ maxFileSize }) }
-					help={ __( 'You may need to contact your hosting provider to increase file sizes.', 'otter-blocks' ) }
-				/>
-
-				<TextControl
-					label={ __( 'Allowed File Types', 'otter-blocks' ) }
-					value={ attributes.allowedFileTypes }
-					onChange={ allowedFileTypes => setAttributes({ allowedFileTypes }) }
-					help={ __( 'Allowed file types separated by coma. E.g.: .png, .jpg, .pdf, .doc', 'otter-blocks' ) }
-				/>
-
-				<TextControl
-					label={ __( 'Help Text', 'otter-blocks' ) }
-					value={ attributes.helpText }
-					onChange={ helpText => setAttributes({ helpText }) }
-				/>
-
-				<ToggleControl
-					label={ __( 'Required', 'otter-blocks' ) }
-					help={ __( 'If enabled, the input field must be filled out before submitting the form.', 'otter-blocks' ) }
-					checked={ attributes.isRequired }
-					onChange={ isRequired => setAttributes({ isRequired }) }
-				/>
-
-				<ToggleControl
-					label={ __( 'Allow multiple file uploads', 'otter-blocks' ) }
-					checked={ attributes.multipleFiles }
-					onChange={ multipleFiles => setAttributes({ multipleFiles }) }
-				/>
-
-				<ToggleControl
-					label={ __( 'Save to Media Library', 'otter-blocks' ) }
-					help={ __( 'If enabled, the files will be saved to Media Library instead of adding them as attachments to email.', 'otter-blocks' ) }
-					checked={ 'media-library' === attributes.saveFiles }
-					onChange={ value => setAttributes({ saveFiles: value ? 'media-library' : undefined }) }
-				/>
-
-				{ ! Boolean( window.themeisleGutenberg?.hasPro ) && (
-					<Notice
-						notice={<ExternalLink href={setUtm( window.themeisleGutenberg.upgradeLink, 'formfilefieldfeature' )}>{__( 'Activate this field with Otter Pro.', 'otter-blocks' )}</ExternalLink>}
-						variant="upsell" instructions={undefined}				/>
-				) }
+				{
+					( ! window.themeisleGutenberg.hasPro ) ? <ProPreview attributes={ attributes } /> : (
+						applyFilters( 'otter.form.file.inspector', <ProPreview attributes={attributes} />, { attributes: attributes, setAttributes: setAttributes })
+					)
+				}
 
 				<div className="o-fp-wrap">
 					{ applyFilters( 'otter.feedback', '', 'sticky' ) }
