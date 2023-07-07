4e7dec012fa28959c6cda9c72403cb44d6af4de7
Soare Robert Daniel (Mac 2023) <soare.robert.daniel@protonmail.com>
2023-07-07 12:59:13 +0300

    chore: lint

--- inc/plugins/class-dashboard.php
+++ inc/plugins/class-dashboard.php
@@ -508,7 +508,7 @@ class Dashboard {
 							url.searchParams.set('otter_form_widget_filter', value);
 							url.searchParams.set('otter_nonce', '<?php echo esc_attr( wp_create_nonce( 'otter_widget_nonce' ) ); ?>')
 							url.hash = '#otter_form_submissions_widget';
-							
+
 							window.location.href = url.href;
 						})
 					}
