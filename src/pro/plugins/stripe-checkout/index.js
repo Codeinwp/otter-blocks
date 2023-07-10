53e4c8324a904e62c90fb23014970f1456477d55
Soare Robert Daniel (Mac 2023) <soare.robert.daniel@protonmail.com>
2023-07-10 13:23:51 +0300

    chore: remove small notice for Stripe autoresponder

--- src/pro/plugins/stripe-checkout/index.js
+++ src/pro/plugins/stripe-checkout/index.js
@@ -48,16 +48,6 @@ const Autoresponder = ( Template, attributes, setAttributes ) => {
 								onChange={ ( body ) => setAttributes({ autoresponder: { ...attributes.autoresponder, body }}) }
 								area="stripe-autoresponder"
 							/>
-
-							{
-								(  ! Boolean( attributes.autoresponder?.title ) && ! Boolean( attributes.autoresponder.body ) ) && (
-									<Notice
-										notice={ __( 'Replace the placeholders for Subject and Body.', 'otter-blocks' ) }
-										instructions={ __( 'You need to fill the Subject field and replace the value of Body of the Autoresponder. Email will not be send if the placeholders are still present.', 'otter-blocks' ) }
-									/>
-								)
-							}
-
 						</Fragment>
 					)
 				}
