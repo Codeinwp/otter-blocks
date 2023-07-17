b64f478ce9d7cf566f926a88730a947fbecd0289
Soare Robert Daniel (Mac 2023) <soare.robert.daniel@protonmail.com>
2023-07-17 16:14:54 +0300

    chore: add prod link

--- inc/server/class-prompt-server.php
+++ inc/server/class-prompt-server.php
@@ -146,7 +146,7 @@ class Prompt_Server {
 				'license_id' => apply_filters( 'product_otter_license_key', 'free' ),
 				'cache'      => gmdate( 'u' ),
 			),
-			'https://qm5dr6t4f4.execute-api.us-east-1.amazonaws.com/default/prompts'
+			'https://api.themeisle.com/templates-cloud/otter-prompts'
 		);
 
 		$response = '';
