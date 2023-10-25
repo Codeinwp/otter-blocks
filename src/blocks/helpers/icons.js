d208055a00349be688a30c5cba2a52b6cdf6b3e2
Soare Robert Daniel (Mac 2023) <soare.robert.daniel@protonmail.com>
2023-10-25 11:30:39 +0300

    chore: styling

--- src/blocks/helpers/icons.js
+++ src/blocks/helpers/icons.js
@@ -658,7 +658,7 @@ export const popupWithImageAndText = (
 );
 
 export const aiGeneration = (
-	<SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" style={{ color: 'transparent' }}>
+	<SVG data-target="generator-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" style={{ color: 'transparent' }}>
 		<Path d="M6 17V21" stroke="url(#paint0_linear_3599_37937)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
 		<Path d="M4 19H8" stroke="url(#paint1_linear_3599_37937)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
 		<Path d="M17 3V7" stroke="url(#paint2_linear_3599_37937)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
