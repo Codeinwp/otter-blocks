23870fe44409cb8dc0da2efb9e557e08af6ab5fe
John Fraskos <jfraskos@gmail.com>
2023-12-13 17:46:59 +0200

    Update Start.js
    Changed text in title and description. Need to add a link to the read more.

--- src/onboarding/components/Start.js
+++ src/onboarding/components/Start.js
@@ -18,8 +18,8 @@ const Start = () => {
 					src={ `${ window.otterOnboardingData.assetsPath }images/logo-alt.png` }
 				/>
 
-				<h1>{ __( 'Welcome to the Theme Onboarding Wizard by Otter.', 'otter-blocks' ) }</h1>
-				<p>{ __( 'This process will help you go through a basic setup of your theme, so you can start customising your site right away. You can trigger this wizard manually at any time, from Appearance Menu.', 'otter-blocks' ) }</p>
+				<h1>{ __( 'Welcome to FSE onboarding, by Otter.', 'otter-blocks' ) }</h1>
+				<p>{ __( 'This process will guide you through a basic setup of your theme, so you can enjoy your new site right away. You can trigger this wizard manually at any time, from Appearance Menu. Learn more.', 'otter-blocks' ) }</p>
 
 				<div className="o-start__actions">
 					<Button
