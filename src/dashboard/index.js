6461a8bd2b4f2921313cdbcad93a12463cfcdb70
Hardeep Asrani <hardeepasrani@gmail.com>
2023-12-14 05:26:42 +0530

    Add NPS Survey

--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -1,3 +1,9 @@
+/* eslint-disable camelcase */
+/**
+ * External dependencies.
+ */
+import formbricks from '@formbricks/js';
+
 /**
  * WordPress dependencies.
  */
@@ -24,11 +30,37 @@ if ( undefined === window.otterUtils ) {
 
 window.otterUtils.useSettings = useSettings;
 
-function getInitialStateFromURLQuery() {
+const getInitialStateFromURLQuery = () => {
 	const hash = window.location.hash.slice( 1 ); // Remove the '#' at the start
 	return hash;
-}
+};
 
+const convertToCategory = ( number, scale = 1 ) => {
+	const normalizedNumber = Math.round( number / scale );
+	if ( 0 === normalizedNumber || 1 === normalizedNumber ) {
+		return 0;
+	} else if ( 1 < normalizedNumber && 8 > normalizedNumber ) {
+		return 7;
+	} else if ( 8 <= normalizedNumber && 31 > normalizedNumber ) {
+		return 30;
+	} else if ( 30 < normalizedNumber && 90 > normalizedNumber ) {
+		return 90;
+	} else if ( 90 > normalizedNumber ) {
+		return 91;
+	}
+};
+
+if ( 'undefined' !== typeof window ) {
+	formbricks.init({
+		environmentId: 'clp9hqm8c1osfdl2ixwd0k0iz',
+		apiHost: 'https://app.formbricks.com',
+		userId: 'otter_' + ( undefined !== window.otterObj?.license?.key ? window.otterObj.license.key : encodeURIComponent( window.otterObj.rootUrl ) ),
+		attributes: {
+			plan: undefined !== window.otterObj?.license?.type ? window.otterObj.license.type : 'free',
+			days_since_install: convertToCategory( window.otterObj.days_since_install )
+		}
+	});
+}
 
 const App = () => {
 	const [ currentTab, setTab ] = useState( getInitialStateFromURLQuery() );
