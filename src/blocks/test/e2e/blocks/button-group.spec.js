5fabe985d5304c6d8cd29a13e30ef379b054d093
Soare Robert Daniel (Mac 2023) <soare.robert.daniel@protonmail.com>
2024-06-19 16:50:31 +0300

    fix: button group font settings

---
+++ src/blocks/test/e2e/blocks/button-group.spec.js
@@ -0,0 +1,44 @@
+/**
+ * WordPress dependencies
+ */
+import { test, expect } from '@wordpress/e2e-test-utils-playwright';
+
+test.describe( 'Button Group', () => {
+	test.beforeEach( async({ admin }) => {
+		await admin.createNewPost();
+	});
+
+	test( 'check font settings', async({ editor, page }) => {
+		const attributes = {
+			fontSize: '28px',
+			fontFamily: 'Abel',
+			textTransform: 'lowercase',
+			fontStyle: 'italic',
+			lineHeight: '2.5'
+		};
+
+		await editor.insertBlock({
+			name: 'themeisle-blocks/button-group',
+			attributes,
+			innerBlocks: [
+				{
+					name: 'themeisle-blocks/button',
+					attributes: {
+						text: 'Button 1'
+					}
+				}
+			]
+		});
+
+		const postId = await editor.publishPost();
+		await page.goto( `/?p=${postId}` );
+
+		// Check CSS font properties
+		const btn = page.locator( 'a' ).filter({ hasText: 'Button 1' });
+		await expect( btn ).toHaveCSS( 'font-size', attributes.fontSize );
+		await expect( btn ).toHaveCSS( 'font-family', attributes.fontFamily );
+		await expect( btn ).toHaveCSS( 'text-transform', attributes.textTransform );
+		await expect( btn ).toHaveCSS( 'font-style', attributes.fontStyle );
+		await expect( btn ).toHaveCSS( 'line-height', `${parseInt( attributes.fontSize ) * parseFloat( attributes.lineHeight )}px` ); // Playwright use computed line-height based on font-size.
+	});
+});
