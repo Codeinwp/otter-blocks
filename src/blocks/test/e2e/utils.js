46a2891f357595e71bb2f65c288514fecb57639e
Soare Robert Daniel (Mac 2023) <soare.robert.daniel@protonmail.com>
2024-06-20 11:48:45 +0300

    chore: js format

--- src/blocks/test/e2e/utils.js
+++ src/blocks/test/e2e/utils.js
@@ -70,11 +70,11 @@ export function deleteFile( filePath ) {
 }
 
 export async function tryLoginIn( page, username = 'admin', password = 'password' ) {
-	await page.goto('/wp-login.php');
-	await page.fill('input[name="log"]', username);
-	await page.fill('input[name="pwd"]', password);
-	await page.check('input[name="rememberme"]');
-	await page.click('input[name="wp-submit"]');
+	await page.goto( '/wp-login.php' );
+	await page.fill( 'input[name="log"]', username );
+	await page.fill( 'input[name="pwd"]', password );
+	await page.check( 'input[name="rememberme"]' );
+	await page.click( 'input[name="wp-submit"]' );
 
 	// Save the context since tests are isolated.
 	// So we need to save the auth state to keep the user logged in so that they can be used in the next test.
