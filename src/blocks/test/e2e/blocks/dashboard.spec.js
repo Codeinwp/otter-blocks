7b597061eae2aaf52b121e6dbf2bb23b648ebdcd
Soare Robert Daniel (Mac 2023) <soare.robert.daniel@protonmail.com>
2024-07-30 14:15:38 +0300

    test(e2e): editable custom actions for AI Toolbar

--- src/blocks/test/e2e/blocks/dashboard.spec.js
+++ src/blocks/test/e2e/blocks/dashboard.spec.js
@@ -8,7 +8,7 @@ test.describe( 'Dashboard', () => {
 		await admin.visitAdminPage( 'admin.php?page=otter' );
 	});
 
-	test( 'toggle AI Block Toolbar', async({ editor, page }) => {
+	test( 'toggle AI Block Toolbar', async({ page }) => {
 
 		const toggle = page.getByLabel( 'Enable AI Block Toolbar Module' );
 		const initialToggleValue = await toggle.isChecked();
@@ -22,5 +22,31 @@ test.describe( 'Dashboard', () => {
 		await toggle.click();
 		await page.waitForTimeout( 1000 );
 		expect( await toggle.isChecked() ).toEqual( initialToggleValue );
+	});
+
+	test( 'edit editable custom actions', async({ page }) => {
+
+		const actionTitleValue = 'Fix Spelling';
+		const actionPromptValue = 'Fix spelling mistakes in the content.';
+
+		await page.getByRole( 'button', { name: 'Integrations' }).click();
+		await page.getByRole( 'button', { name: 'OpenAI' }).click();
+
+		// Edit the first action.
+		page.locator( '.otter-ai-toolbar-actions .components-panel__body:first-child button' ).click();
+		await page.locator( '.otter-ai-toolbar-actions .components-panel__body:first-child' ).getByPlaceholder( 'Action Name' ).fill( actionTitleValue );
+		await page.locator( '.otter-ai-toolbar-actions .components-panel__body:first-child' ).getByPlaceholder( 'Prompt' ).fill( actionPromptValue );
+
+		// Save the changes.
+		const requestPromise = page.waitForRequest( request =>
+			request.url().includes( 'settings' )
+		);
+		await page.locator( 'div' ).filter({ hasText: /^SaveMore Info↗$/ }).getByRole( 'button' ).click();
+		await requestPromise;
+		await page.reload();
+
+		await page.getByRole( 'button', { name: 'Integrations' }).click();
+		await page.getByRole( 'button', { name: 'OpenAI' }).click();
+		await expect( page.getByRole( 'button', { name: actionTitleValue }) ).toBeVisible();
 	});
 });
