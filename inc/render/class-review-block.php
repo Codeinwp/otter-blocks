8990a37d13335eb8d2f2b1b2645c9e7bbc6b39aa
Soare Robert Daniel (Mac 2023) <soare.robert.daniel@protonmail.com>
2024-10-07 17:07:31 +0300

    fix: string translation

--- inc/render/class-review-block.php
+++ inc/render/class-review-block.php
@@ -85,8 +85,12 @@ class Review_Block {
 		$html .= '			    <div class="o-review__header_ratings__stars">';
 		$html .= $this->get_overall_stars( $this->get_overall_ratings( $attributes['features'] ), $scale );
 		$html .= '			    </div>';
-		// translators: Overall rating from 1 to 10.
-		$html .= '				<span>' . sprintf( __( '%1$g out of %2$g', 'otter-blocks' ), $this->get_overall_ratings( $attributes['features'], $scale ), 10 / $scale ) . '</span>';
+		$html .= '				<span>' . sprintf(
+			// translators: %1$g is the overall rating, %2$g is the maximum rating.
+			__( '%1$g out of %2$g', 'otter-blocks' ),
+			$this->get_overall_ratings( $attributes['features'], $scale ), 
+			10 / $scale 
+		) . '</span>';
 		$html .= '			</div>';
 
 		if ( ( isset( $attributes['price'] ) && ! empty( $attributes['price'] ) ) || isset( $attributes['discounted'] ) ) {
@@ -133,9 +137,12 @@ class Review_Block {
 				$html .= '         <div class="o-review__left_feature_ratings__stars">';
 				$html .= $this->get_overall_stars( $feature['rating'], $scale );
 				$html .= '         </div>';
-
-				// translators: Overall rating from 1 to 10.
-				$html .= '			<span class="o-review__left_feature_num">' . sprintf( __( '%1$g out of %2$g', 'otter-blocks' ), 1 <= round( $feature['rating'] / $scale, 1 ) ? round( $feature['rating'] / $scale, 1 ) : 1, 10 / $scale ) . '</span>';
+				$html .= '			<span class="o-review__left_feature_num">' . sprintf(
+					// translators: %1$g is the overall rating, %2$g is the maximum rating.
+					__( '%1$g out of %2$g', 'otter-blocks' ),
+					1 <= round( $feature['rating'] / $scale, 1 ) ? round( $feature['rating'] / $scale, 1 ) : 1, 
+					10 / $scale
+				) . '</span>';
 
 				$html .= '		</div>';
 
