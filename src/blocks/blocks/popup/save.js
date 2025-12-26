1ee4aa501f49e869664a8c3cdeaba01d5d8bf02a
girishpanchal30 <girish@krishaweb.com>
2025-12-26 10:49:40 +0530

    fix: add title to close button

--- src/blocks/blocks/popup/save.js
+++ src/blocks/blocks/popup/save.js
@@ -1,6 +1,8 @@
 /**
  * WordPress dependencies
  */
+import { __ } from '@wordpress/i18n';
+
 import {
 	InnerBlocks,
 	useBlockProps
@@ -36,7 +38,7 @@ const Save = ({
 				<div className="otter-popup__modal_content">
 					{ attributes.showClose && (
 						<div className="otter-popup__modal_header">
-							<button type="button" className="components-button has-icon"><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" role="img" aria-hidden="true"><path d="M12 13.06l3.712 3.713 1.061-1.06L13.061 12l3.712-3.712-1.06-1.06L12 10.938 8.288 7.227l-1.061 1.06L10.939 12l-3.712 3.712 1.06 1.061L12 13.061z"></path></svg></button>
+							<button title={ __( 'Close Button', 'otter-blocks' ) } type="button" className="components-button has-icon"><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" role="img" aria-hidden="true"><path d="M12 13.06l3.712 3.713 1.061-1.06L13.061 12l3.712-3.712-1.06-1.06L12 10.938 8.288 7.227l-1.061 1.06L10.939 12l-3.712 3.712 1.06 1.061L12 13.061z"></path></svg></button>
 						</div>
 					) }
 
